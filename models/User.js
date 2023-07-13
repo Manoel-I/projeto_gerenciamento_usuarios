47cef6b7be7f41f90419742f70bc4342e91d741d
Manoel-I <manoel.122112@outlook.com>
2023-07-12 22:04:56 -0300

    Check if email already exists

--- models/User.js
+++ models/User.js
@@ -4,10 +4,27 @@ const bcrypt = require('bcrypt');
 class User{
     async new_user(name, email, password){
         try{
-            await knex.insert({name, email, password}).table('users');
+            let hash = bcrypt.hash(password, 10);
+            await knex.insert({name, email, password: hash}).table('users');
         }catch(error){
             console.log(error);
         }
+    }
+
+    async find_email(email){
+        try{
+            let result;
+            await knex.select().table('users').where({email : email});
+            if(result.length > 0){
+                return true;
+            }else{
+                return false;
+            }
+        }catch(error){
+            console.log(error);
+        }
+        
+        
     }
 }
 
